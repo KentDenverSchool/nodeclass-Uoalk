f0a8ce6f8a860ac714e69ca526b7987eae8a0e13
Grant Fitez <grantfitez@gmail.com>
2018-08-23 19:23:12 -0600

    Added driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,20 @@
-
+import java.util.ArrayList;
+import static java.lang.System.out;
 public class Driver {
+    public static void main(String[] args) {
+        Node a=new Node("Hi", new ArrayList<>());
+
+        Node b=new Node("Hello", new ArrayList<>());
+        Node c=new Node("Test", new ArrayList<>());
+
+        System.out.println(a);
+
+        a.addConnection(b);
+        a.addConnection(c);
+        System.out.println(a);
+
+        b.addConnection(c);
+        System.out.println(b);
+    }
+
 }
